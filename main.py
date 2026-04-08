4ff8263c4e714278efb0e00128743e5cd4e8e54e
Sherezz Grant <rezziemaven@gmail.com>
2026-04-08 11:58:25 -0400

    Draw player on screen

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     clock = pygame.time.Clock()
     dt = 0  # tracks delta time
 
@@ -22,6 +25,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
