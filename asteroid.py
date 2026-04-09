4d9b014af438b7d880bfed70244a0de49337c67c
Sherezz Grant <rezziemaven@gmail.com>
2026-04-08 22:06:29 -0400

    Add asteroid split method This method splits the asteroid up into two new asteroids moving in random directions, increasing their velocities. They are removed by the player once small enough.

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        asteroid1_velocity = self.velocity.rotate(angle)
+        asteroid2_velocity = self.velocity.rotate(angle * -1)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid2 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid1.velocity = asteroid1_velocity * 1.2
+        asteroid2.velocity = asteroid2_velocity * 1.2
